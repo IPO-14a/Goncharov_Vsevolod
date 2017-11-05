e0b45846ace978ca861e2216c8f0e0a12c4d9da7
Vsevolod <31592913+Vsevood@users.noreply.github.com>
2017-11-05 20:11:27 +0300

    3.1.2 Заполнение функции contains

--- tic-tac-toe/js/tic-tac-toe.js
+++ tic-tac-toe/js/tic-tac-toe.js
@@ -41,7 +41,10 @@ function win(clicked) {
 }
 
 function contains(selector, text) {
-
+var elements = document.querySelectorAll(selector);
+  return [].filter.call(elements, function (element) {
+    return RegExp(text).test(element.textContent);
+  });
 }
 
 function set() {
