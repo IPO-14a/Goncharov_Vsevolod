6fc1723d8c6174aeb28c6d810a3245fef229e888
Vsevolod <31592913+Vsevood@users.noreply.github.com>
2017-11-05 19:41:58 +0300

    2.4 Заполненна функция startNewGame

--- tic-tac-toe/js/tic-tac-toe.js
+++ tic-tac-toe/js/tic-tac-toe.js
@@ -10,7 +10,15 @@ function init() {
 }
 
 function startNewGame() {
-
+  score = {
+    'X': 0,
+    'O': 0
+  };
+  moves = 0;
+  turn = 'X';
+  boxes.forEach(function (square) {
+    square.innerHTML = EMPTY;
+  });
 }
 
 function win(clicked) {
