812aeb171bca830dc9d8ebee0ff12a4c00b55834
Goncharov Cyril <Krio05@yandex.ua>
2017-11-07 08:53:30 +0200

    Правки в коде

--- tic-tac-toe/js/tic-tac-toe.js
+++ tic-tac-toe/js/tic-tac-toe.js
@@ -1,96 +1,110 @@
-var N_SIZE = 3, //размер поля
-  EMPTY = '&nbsp;',
-  boxes = [],
-  turn = 'X',
-  score,
-  moves;
+/*
+*параметры программы
+*/
+var N_SIZE = 3,
+var EMPTY = '&nbsp;',
+var boxes = [],
+var turn = 'X',
+var score,
+var moves;
 
+/*
+*главнвя функция
+*/
 function init() {
-  // Главная функция
-  var board = document.createElement('table');
-  board.setAttribute('border', 1);
-  board.setAttribute('cellspacing', 0);
+    var board = document.createElement('table');
+    board.setAttribute('border', 1);
+    board.setAttribute('cellspacing', 0);
 
-  var identifier = 1;
-  for (var i = 0; i < N_SIZE; i++) {
-    var row = document.createElement('tr');
-    board.appendChild(row);
-    for (var j = 0; j < N_SIZE; j++) {
-      var cell = document.createElement('td');
-      cell.setAttribute('height', 120);
-      cell.setAttribute('width', 120);
-      cell.setAttribute('align', 'center');
-      cell.setAttribute('valign', 'center');
-      cell.classList.add('col' + j, 'row' + i);
-      if (i == j) {
-        cell.classList.add('diagonal0');
-      }
-      if (j == N_SIZE - i - 1) {
-        cell.classList.add('diagonal1');
-      }
-      cell.identifier = identifier;
-      cell.addEventListener('click', set);
-      row.appendChild(cell);
-      boxes.push(cell);
-      identifier += identifier;
+    var identifier = 1;
+    for (var i = 0; i < N_SIZE; i++) {
+        var row = document.createElement('tr');
+        board.appendChild(row);
+        for (var j = 0; j < N_SIZE; j++) {
+            var cell = document.createElement('td');
+            cell.setAttribute('height', 120);
+            cell.setAttribute('width', 120);
+            cell.setAttribute('align', 'center');
+            cell.setAttribute('valign', 'center');
+            cell.classList.add('col' + j, 'row' + i);
+            if (i == j) {
+                cell.classList.add('diagonal0');
+            }
+            if (j == N_SIZE - i - 1) {
+                cell.classList.add('diagonal1');
+            }
+            cell.identifier = identifier;
+            cell.addEventListener('click', set);
+            row.appendChild(cell);
+            boxes.push(cell);
+            identifier += identifier;
+        }
     }
-  }
 
-  document.getElementById('tictactoe').appendChild(board);
-  startNewGame();
+    document.getElementById('tictactoe').appendChild(board);
+    startNewGame();
 }
 
+/*
+*Фунцкия старта новой игры
+*/
 function startNewGame() {
-  // Функция начала новой игры
-  score = {
-    'X': 0,
-    'O': 0
-  };
-  moves = 0;
-  turn = 'X';
-  boxes.forEach(function (square) {
-    square.innerHTML = EMPTY;
-  });
+    score = {
+        'X': 0,
+        'O': 0
+    };
+    moves = 0;
+    turn = 'X';
+    boxes.forEach(function (square) {
+        square.innerHTML = EMPTY;
+    });
 }
 
+/*
+*функция определяющая победу
+*/
 function win(clicked) {
-  // Функция определяющая победу
-  var memberOf = clicked.className.split(/\s+/);
-  for (var i = 0; i < memberOf.length; i++) {
-    var testClass = '.' + memberOf[i];
-    var items = contains('#tictactoe ' + testClass, turn);
-    if (items.length == N_SIZE) {
-      return true;
+    var memberOf = clicked.className.split(/\s+/);
+    for (var i = 0; i < memberOf.length; i++) {
+        var testClass = '.' + memberOf[i];
+        var items = contains('#tictactoe ' + testClass, turn);
+        if (items.length == N_SIZE) {
+            return true;
+        }
     }
-  }
-  return false;
+    return false;
 }
 
+/*
+*функция проверки символа в ячейке
+*/
 function contains(selector, text) {
-  var elements = document.querySelectorAll(selector);
-  return [].filter.call(elements, function (element) {
-    return RegExp(text).test(element.textContent);
-  });
+    var elements = document.querySelectorAll(selector);
+    return [].filter.call(elements, function (element) {
+        return RegExp(text).test(element.textContent);
+    });
 }
 
+/*
+*функция заполнения ячейки
+*/
 function set() {
-  // Функция определения победителя
-  if (this.innerHTML !== EMPTY) {
-    return;
-  }
-  this.innerHTML = turn;
-  moves += 1;
-  score[turn] += this.identifier;
-  if (win(this)) {
-    alert('Winner: Player ' + turn);
-    startNewGame();
-  } else if (moves === N_SIZE * N_SIZE) {
-    alert('Draw');
-    startNewGame();
-  } else {
-    turn = turn === 'X' ? 'O' : 'X';
-    document.getElementById('turn').textContent = 'Player ' + turn;
-  }
+    if (this.innerHTML !== EMPTY) {
+        return;
+    }
+    this.innerHTML = turn;
+    moves += 1;
+    score[turn] += this.identifier;
+    if (win(this)) {
+        alert('Winner: Player ' + turn);
+        startNewGame();
+    } else if (moves === N_SIZE * N_SIZE) {
+        alert('Draw');
+        startNewGame();
+    } else {
+        turn = turn === 'X' ? 'O' : 'X';
+        document.getElementById('turn').textContent = 'Player ' + turn;
+    }
 }
 
 init();
